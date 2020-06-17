6e6d27cacfeb7e8006204ddc5921c2c199b907d2
Nadav Daester <daesternadav@gmail.com>
2020-06-17 19:26:25 +0300

    enabled api

--- travel-tip-startHere/js/services/map.service.js
+++ travel-tip-startHere/js/services/map.service.js
@@ -9,6 +9,19 @@ export const mapService = {
 var map;
 
 
+// function initMap() {
+//     // The location of Uluru
+//     var uluru = {lat: -25.344, lng: 131.036};
+//     // The map, centered at Uluru
+//     var map = new google.maps.Map(
+//         document.getElementById('map'), {zoom: 4, center: uluru});
+//     // The marker, positioned at Uluru
+//     var marker = new google.maps.Marker({position: uluru, map: map});
+//   }
+//  var src=`https://maps.googleapis.com/maps/api/js?${AIzaSyDaI_zceRRvGht5JxxikelDhi1uM5urmx4}&callback=initMap`>
+
+
+
 export function initMap(lat = 32.0749831, lng = 34.9120554) {
     console.log('InitMap');
     return _connectGoogleApi()
@@ -22,6 +35,8 @@ export function initMap(lat = 32.0749831, lng = 34.9120554) {
             console.log('Map!', map);
         })
 }
+
+
 
 function addMarker(loc) {
     var marker = new google.maps.Marker({
@@ -39,7 +54,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyAG9lyiCrp2ect6yrOqv2yzKS9jgUNay3g'; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
@@ -50,6 +65,3 @@ function _connectGoogleApi() {
         elGoogleApi.onerror = () => reject('Google script failed to load')
     })
 }
-
-
-
